d80e40df047b239c5b99836ad3ddbf5a52c3ed2a
Le Tho <thold88@gmail.com>
2020-11-09 15:01:28 +0700

    Init repository

---
+++ site/components/src/main/java/uk/co/manifesto/brxm/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package uk.co.manifesto.brxm.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="spaintegration:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}

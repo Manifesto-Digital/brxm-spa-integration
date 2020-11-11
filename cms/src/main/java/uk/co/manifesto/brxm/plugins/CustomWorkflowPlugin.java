9c029791e807fa01dddb8fdf9f34fd7044ff60bb
Le Tho <thold88@gmail.com>
2020-11-11 16:18:03 +0700

    Add custom menu item

---
+++ cms/src/main/java/uk/co/manifesto/brxm/plugins/CustomWorkflowPlugin.java
@@ -0,0 +1,34 @@
+package uk.co.manifesto.brxm.plugins;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.model.StringResourceModel;
+import org.hippoecm.addon.workflow.StdWorkflow;
+import org.hippoecm.addon.workflow.WorkflowDescriptorModel;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugins.standards.icon.HippoIcon;
+import org.hippoecm.frontend.service.render.RenderPlugin;
+import org.hippoecm.frontend.skin.Icon;
+import org.hippoecm.repository.api.WorkflowDescriptor;
+import org.hippoecm.repository.standardworkflow.FolderWorkflow;
+
+public class CustomWorkflowPlugin extends RenderPlugin<WorkflowDescriptor> {
+    public CustomWorkflowPlugin(IPluginContext context, IPluginConfig config) {
+        super(context, config);
+
+        add(new StdWorkflow<FolderWorkflow>("index",
+                new StringResourceModel("custom_action", this),
+                (WorkflowDescriptorModel) getModel())  {
+            @Override
+            protected Component getIcon(final String id) {
+                return HippoIcon.fromSprite(id, Icon.FILE);
+            }
+
+            @Override
+            protected String execute(FolderWorkflow workflow) throws Exception {
+                workflow.add("new-document", "spaintegration:newsdocument", "index");
+                return null;
+            }
+        });
+    }
+}
